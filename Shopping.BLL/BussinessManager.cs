3d0703d415d49af6165aa86c063e92f94cdf0af7
semvs <semvs@ayvansaray.edu.tr>
2019-02-14 11:34:15 +0300

    bll eklendi

--- Shopping.BLL/BussinessManager.cs
+++ Shopping.BLL/BussinessManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shopping.BLL.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,8 +12,31 @@ namespace Shopping.BLL
     {
         public class CategoriesManager : CategoriesRepository
         {
+            public List<CategoriesDTOs> KategoriListele()
+            {
+                return GenelListe().Select(x => new CategoriesDTOs {
+                    CategoryId=x.CategoryID,
+                    CategoryName=x.CategoryName
 
+                }).ToList();
+            }
 
         }
+        public class ProductsManager : ProductsRepository
+        {
+            public List<ProductsDTO> UrunListele() {
+
+                return GenelListe().Select(x=>new ProductsDTO {
+                    ProductsId=x.ProductID,
+                    CategoryId=(int)x.CategoryID,
+                    ProductName=x.ProductName,
+                    UnitPrice=(decimal)x.UnitPrice
+                    
+                }).ToList();
+               
+            }
+
+        }
+
     }
 }
